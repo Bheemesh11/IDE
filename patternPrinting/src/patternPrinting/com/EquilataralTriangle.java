4afb36d5efe222b9757755c530345bb20d20c633
Bheemajetty <mrbheem11@gmail.com>
2024-05-02 11:14:57 +0530

    patternPrint

---
+++ patternPrinting/src/patternPrinting/com/EquilataralTriangle.java
@@ -0,0 +1,27 @@
+package patternPrinting.com;
+/*Write a program to display EQUILATERAL TRIANGLE with stars
+	*
+   ***
+  *****
+ *******
+*********
+
+*/
+
+public class EquilataralTriangle {
+
+	public static void main(String[] args) {
+		int n = 7;
+		for (int i = 1; i <= n; i++) {
+		 for (int j = 1; j <= n-i; j++) {
+			System.out.print(" ");
+		}	
+		 for (int k = 1; k<=2*i-1; k++) {
+			 System.out.print("*");
+		}
+		 System.out.println();
+		}
+		
+		
+	}
+}
---
+++ patternPrinting/src/patternPrinting/com/InvertedTriangle.java
@@ -0,0 +1,5 @@
+package patternPrinting.com;
+//Write a program to Display INVERTED TRIANGLE with stars?
+public class InvertedTriangle {
+
+}
